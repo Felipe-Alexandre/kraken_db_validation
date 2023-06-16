123e6315cb344b4208ffd4230bcfe3ed10115328
Felipe Alexandre Barbosa <felipe.as.barbosa99@gmail.com>
2023-06-16 17:20:11 -0300

    update final panel output

--- scripts/plot_panel.r
+++ scripts/plot_panel.r
@@ -2,8 +2,9 @@ top_panel <- plot_grid(plot3, plot4, plot2, ncol = 1)
 top_panel
 eco_legend <- cowplot::get_legend(plot2)
 
-#Saave plot
+#Save plot
 ggsave("outputs/top_panel.png", width = 19, height = 18, units = "cm")
+ggsave("outputs/top_panel.svg", width = 19, height = 18, units = "cm")
 
 #Removing legend
 plot2 <- plot2 + theme(legend.position = "none")
